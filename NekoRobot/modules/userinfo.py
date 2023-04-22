fd68c2013bc062abcb25e4a7d6432b5c9e46fbce
Haruki <127585577+Aaaakkkkbot@users.noreply.github.com>
2023-04-22 16:51:15 +0530

    Update userinfo.py

--- NekoRobot/modules/userinfo.py
+++ NekoRobot/modules/userinfo.py
@@ -45,7 +45,7 @@ from NekoRobot.modules.sql.afk_sql import check_afk_status, is_afk
 from NekoRobot.modules.sql.global_bans_sql import is_user_gbanned
 from NekoRobot.modules.sql.users_sql import get_user_num_chats
 
-NEKO_IMG = "https://telegra.ph/file/a21731c0c4c7f27a3ec16.jpg"
+NEKO_IMG = "https://telegra.ph/file/7a0e166cbe8126a9c77c3.jpg"
 
 
 def no_by_per(totalhp, percentage):
@@ -238,10 +238,10 @@ def info(update: Update, context: CallbackContext):
     buttons = [
         [
             InlineKeyboardButton(
-                text="Health", url="https://t.me/Programmer_Updates/10"
+                text="Bots🚨", url="https://t.me/AeroBots_Tm/10"
             ),
             InlineKeyboardButton(
-                text="Disasters", url="https://t.me/Programmer_Updates/11"
+                text="Support⚡", url="https://t.me/AeroBots_Group"
             ),
         ],
     ]
